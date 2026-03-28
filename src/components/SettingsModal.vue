58c96857dea2e52db8f305fffdcaad33f52fe694
Kaspar Martin Suursalu <kasparsu@gmail.com>
2026-03-28 09:02:23 +0200

    Update new name for download

--- src/components/SettingsModal.vue
+++ src/components/SettingsModal.vue
@@ -241,7 +241,7 @@ function exportData() {
   const url  = URL.createObjectURL(blob)
   const a    = document.createElement('a')
   a.href     = url
-  a.download = `nicquitin-${new Date().toISOString().slice(0, 10)}.json`
+  a.download = `niczero-${new Date().toISOString().slice(0, 10)}.json`
   a.click()
   URL.revokeObjectURL(url)
 }
